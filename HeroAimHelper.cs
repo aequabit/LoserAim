c864a4b597774618d0dbd2a4145fe5a78842d8bc
aequabit <contact@aequabit.de>
2020-03-31 09:45:54 +0200

    Initial commit

---
+++ HeroAimHelper.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoserAim
+{
+    class HeroAimHelper
+    {
+        public const string HEROAIM_SERIAL_KEY = "COHXCETYPSSNDEUTECYGAQLLVRYXFYEHKQGWVTXTDSHRGRAFRNJECQRMDVAKGWTQ";
+        public const string HEROAIM_XOR_KEY = "r8kVu6wgbWeSllDFd0NedIjdFC4UMYkUm8D8x6XNeTHCDfG30y4P3cb0QGwlUzaq";
+        public const string HEROAIM_CSGO_PAYLOAD = "https://heroaim.com/stage/patch2";
+        public const string HEROAIM_SUPPORTED_VERSION = "170";
+
+        private static WebClient _webClient = new WebClient();
+
+        public static string Version = null;
+
+        public static void InjectCsgoCheat(string loaderPath)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = loaderPath;
+            process.StartInfo.Arguments = "";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+            process.WaitForExit();
+
+            try
+            {
+                if (File.Exists(loaderPath))
+                {
+                    File.Delete(loaderPath);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public static void DownloadCsgoPayload(string outputPath)
+        {
+            // Download the CS:GO payload
+            _webClient.DownloadFile(HEROAIM_CSGO_PAYLOAD, outputPath);
+        }
+
+        public static void ExtractCsgoPayload(string blobPath, string dllOutputPath, string injectorOutputPath)
+        {
+            // XOR the serial key
+            var serialKey = HeroAimPro.StringCipher.XORCipher(HEROAIM_SERIAL_KEY, HEROAIM_XOR_KEY);
+
+            // Extract the CS:GO DLL and injector from the payload
+            HeroAimPro.Client.UnpackPayload(
+                blobPath, // path to encrypted blob
+                dllOutputPath, // dll output path
+                injectorOutputPath, // injector output path
+                serialKey); // serial key to decrypt with
+        }
+
+        public static bool WriteRegistry(string key, string value)
+        {
+            var registryKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\HeroAim");
+
+            if (registryKey == null)
+            {
+                return false;
+            }
+
+            registryKey.SetValue(key, value);
+            
+            registryKey.Close();
+
+            return true;
+        }
+
+        public static string DownloadSettings()
+        {
+            return _webClient.DownloadString("https://heroaim.com/version");
+        }
+
+        public static void UpdateSettings(string settings)
+        {
+            string[] array = settings.Split(new char[]
+            {
+                ','
+            });
+
+            int num = (array.Length - 1) / 2;
+
+            try
+            {
+                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("Software\\HeroAim", true);
+
+                for (int i = 0; i < num; i++)
+                {
+                    registryKey.SetValue(array[1 + i * 2], array[2 + i * 2]);
+                }
+
+                registryKey.Close();
+            }
+            catch
+            {
+            }
+
+            Version = array[0];
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HeroAimPro;
+
+namespace LoserAim
+{
+    class Program
+    {
+        private static bool YesNoPrompt()
+        {
+            ConsoleKey response;
+
+            do
+            {
+                response = Console.ReadKey(true).Key;
+            } while (response != ConsoleKey.Y && response != ConsoleKey.N);
+
+            return response == ConsoleKey.Y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "LoserAim lol kek";
+            Console.WriteLine("LoserAim for HeroAim CS:GO - made by aequabit, NotOfficer, YatoDev and RaZa");
+            Console.WriteLine("-");
+
+            // Check for CS:GO
+            if (Process.GetProcessesByName("csgo").Length < 1)
+            {
+                Console.WriteLine("> Please start CS:GO first.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Payload output path
+            var dllPath = Path.Combine(Environment.CurrentDirectory, "heroaim_csgo.dll");
+            var injectorPath = Path.Combine(Environment.CurrentDirectory, "injector.exe");
+
+            // Write registry values for the injector and cheat
+            if (!HeroAimHelper.WriteRegistry("ClientState", "1") ||
+                !HeroAimHelper.WriteRegistry("Install", Environment.CurrentDirectory) ||
+                !HeroAimHelper.WriteRegistry("UtilityLocation", dllPath) ||
+                !HeroAimHelper.WriteRegistry("HeroAimProInjected", "1"))
+            {
+                Console.WriteLine("> ERROR: Failed to write to registry. Try restarting as administrator.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Asset paths
+            var blobPath = Path.Combine(Environment.CurrentDirectory, "heroaim_csgo_blob.bin");
+            var versionPath = Path.Combine(Environment.CurrentDirectory, "version");
+            
+            var version = "";
+            if (!File.Exists(versionPath))
+            {
+                Console.WriteLine("> ERROR: Failed to read version information.");
+                Console.ReadKey();
+                return;
+            }
+            version = File.ReadAllText(versionPath);
+
+            Console.WriteLine("> Would you like to download a current version of HeroAim? [y/n]");
+            Console.WriteLine("> WARNING: Newer versions might be unsafe and contain spyware!");
+            
+            // Get user input
+            var doUpdate = YesNoPrompt();
+
+            if (doUpdate)
+            {
+                blobPath = Path.Combine(Path.GetTempPath(), "heroaim_csgo_blob.bin");
+
+                Console.Write("> Downloading client settings...");
+
+                // Download client settings
+                version = HeroAimHelper.DownloadSettings();
+
+                Console.WriteLine("ok");
+
+                Console.Write("> Downloading HeroAim CS:GO...");
+
+                try
+                {
+                    // Download the CS:GO payload
+                    HeroAimHelper.DownloadCsgoPayload(blobPath);
+
+                    Console.WriteLine("ok");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("err");
+                    Console.WriteLine("> ERROR: Failed to download CS:GO payload.");
+                }
+            }
+
+            Console.Write("> Updating client settings...");
+
+            // Download client settings
+            HeroAimHelper.UpdateSettings(version);
+
+            Console.WriteLine("ok");
+
+            // Check for cheat updates
+            if (HeroAimHelper.Version != HeroAimHelper.HEROAIM_SUPPORTED_VERSION)
+            {
+                Console.WriteLine("> WARNING: This emulator version does not support the current HeroAim version. Would you like to continue anyways? [y/n]");
+                
+                if (!YesNoPrompt())
+                {
+                    Console.WriteLine("Press any key to close...");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            Console.Write("> Extracting HeroAim CS:GO...");
+
+            try
+            {
+                // Extract the CS:GO payload
+                HeroAimHelper.ExtractCsgoPayload(blobPath, dllPath, injectorPath);
+
+                Console.WriteLine("ok");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("err");
+                Console.WriteLine("> ERROR: Failed to extract CS:GO payload.");
+            }
+
+            Console.Write("> Injecting...");
+
+            // Inject the CS:GO cheat
+            HeroAimHelper.InjectCsgoCheat(injectorPath);
+
+            Console.WriteLine("ok");
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
